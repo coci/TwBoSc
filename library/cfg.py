f6556287e2d40ca3a7dafbbb1398a81580b459a7
soroush safari <soroush_safarii@yahoo.com>
2018-06-29 15:34:57 +0430

    fix

--- library/cfg.py
+++
@@ -1,4 +0,0 @@
-consumer_key        = 'QY21t4cnZr8nFIT6ppMFlInxN'
-consumer_secret     = 'dT4yMtQJ7vwb8U1qwWNQ9dKfBucv9Gqi19A0tE56fySW8zk78T'
-access_token        = '813640679704428544-2Vd6qDb8cNczd2sD5EGb1bZ4EreAe98'
-access_token_secret = 'oPy04Qau5cIVOInl8jJjhy9BTTHUB5TPd3A5JCaTwQG5x'
